c354601c7c9cc5dd0197b9e891093d60ef2b7fb2
david <david_noakes@dialog.com.au>
2014-08-22 17:15:16 +1000

    add in the command line utility

---
+++ ExampleSrc/java/org/onebusway/cli/CommandLineInterfaceLibrary.java
@@ -0,0 +1,56 @@
+**
+ * Copyright (C) 2012 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onebusaway.cli;
+ 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+ 
+public class CommandLineInterfaceLibrary {
+ 
+  public static boolean wantsHelp(String[] args) {
+    if (args.length == 0) {
+      return false;
+    }
+    String first = args[0].toLowerCase();
+    while (first.startsWith("-")) {
+      first = first.substring(1);
+    }
+    return first.equals("h") || first.equals("help") || first.equals("?");
+  }
+ 
+  public static void printUsage(Class<?> mainClass) {
+    InputStream is = mainClass.getResourceAsStream("usage.txt");
+    BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+    String line = null;
+    try {
+      while ((line = reader.readLine()) != null) {
+        System.err.println(line);
+      }
+    } catch (IOException ex) {
+ 
+    } finally {
+      if (reader != null) {
+        try {
+          reader.close();
+        } catch (IOException ex) {
+ 
+        }
+      }
+    }
+  }
+}
